97571fea3a2dbad9853c06be04a19dc681d32d75
YS <ribe1918s@gmail.com>
2020-12-07 23:07:53 +0900

    change setting electron

--- public/main.js
+++ public/main.js
@@ -1,22 +1,35 @@
-var _a = require('electron'), app = _a.app, BrowserWindow = _a.BrowserWindow;
+"use strict";
+exports.__esModule = true;
+var electron_1 = require("electron");
+var isDev = require("electron-is-dev");
+var path = require("path");
 function createWindow() {
-    var win = new BrowserWindow({
+    var mainWindow = new electron_1.BrowserWindow({
         width: 800,
         height: 600,
+        center: true,
+        kiosk: !isDev,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
+            contextIsolation: true
         }
     });
-    win.loadFile('index.html');
+    if (isDev) {
+        mainWindow.loadURL('http://localhost:3000');
+        mainWindow.webContents.openDevTools();
+    }
+    else {
+        mainWindow.loadFile(path.join(__dirname, './public/index.html'));
+    }
 }
-app.whenReady().then(createWindow);
-app.on('window-all-closed', function () {
+electron_1.app.whenReady().then(createWindow);
+electron_1.app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
-        app.quit();
+        electron_1.app.quit();
     }
 });
-app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) {
+electron_1.app.on('activate', function () {
+    if (electron_1.BrowserWindow.getAllWindows().length === 0) {
         createWindow();
     }
 });
--- public/main.ts
+++ public/main.ts
@@ -1,15 +1,25 @@
-const { app, BrowserWindow } = require('electron')
+import { app, BrowserWindow } from 'electron'
+import * as isDev from 'electron-is-dev'
+import * as path from 'path'
 
 function createWindow () {
-  const win = new BrowserWindow({
+  const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    center: true,
+    kiosk: !isDev,
     webPreferences: {
-      nodeIntegration: true
-    }
+      nodeIntegration: true,
+      contextIsolation: true,
+    },
   })
 
-  win.loadFile('index.html')
+  if (isDev) {
+    mainWindow.loadURL('http://localhost:3000')
+    mainWindow.webContents.openDevTools()
+  } else {
+    mainWindow.loadFile(path.join(__dirname, './public/index.html'))
+  }
 }
 
 app.whenReady().then(createWindow)
