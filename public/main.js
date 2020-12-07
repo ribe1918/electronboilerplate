768a00df5b5103176bbf2cc92b42167c10414dd7
ribe <ribe1918s@gmail.com>
2020-12-07 16:47:45 +0900

    Initialize Commit

---
+++ public/main.js
@@ -0,0 +1,22 @@
+var _a = require('electron'), app = _a.app, BrowserWindow = _a.BrowserWindow;
+function createWindow() {
+    var win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    win.loadFile('index.html');
+}
+app.whenReady().then(createWindow);
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
